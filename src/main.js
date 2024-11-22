8de1e0d48ac644a024714b596cf334be567e9034
ALavrinovich <teacher.artem@yandex.ru>
2024-11-22 14:49:44 +0300

    fix: update paths and base URL for GitHub Pages

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import App from './App.svelte'
 const target = document.getElementById('app')
 if (!target) {
   console.error('Target element #app not found!')
-} else {
-  const app = new App({
-    target,
-  })
-
-  export default app
 }
+
+const app = new App({
+  target,
+})
+
+export default app
